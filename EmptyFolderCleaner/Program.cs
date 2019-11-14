c7d15fb5bbc179cbc10d5bc112d2941d6bf13122
artoomen <aratrollzzz@gmail.com>
2019-11-14 06:19:41 +0300

    Add Delete and CreateTestDirectories.

--- EmptyFolderCleaner/Program.cs
+++ EmptyFolderCleaner/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace EmptyFolderCleaner
 {
@@ -9,10 +11,73 @@ namespace EmptyFolderCleaner
         {
             var path = Environment.CurrentDirectory;
             Console.WriteLine(path);
-            var children = Directory.GetDirectories(path);
-            PrintEmptyDirectoriesRecursive(path);
+
+            if (args.Any(a => a.Equals("-test", StringComparison.OrdinalIgnoreCase))) 
+            { 
+                //Если в агрументах прописан ключ "-test".
+                CreateTestDirectories(path);
+                return; 
+            }
+
+            var emptyDirectories = new List<string>();
+            do
+            {
+                emptyDirectories.Clear();
+                FillEmptyDirectoriesRecursive(path, emptyDirectories);
+                foreach (var dir in emptyDirectories)
+                {
+                    var dirInfo = new DirectoryInfo(dir);
+                    dirInfo.Attributes = FileAttributes.Normal; //Для невидимых и read-only папок.
+                    Directory.Delete(dir, recursive: true);
+                    Console.WriteLine(dir);
+                }
+            } while (emptyDirectories.Count != 0);
+        }
+
+        static void CreateTestDirectories(string path)
+        {
+            Directory.CreateDirectory(Path.Combine(path, "1"));
+            Directory.CreateDirectory(Path.Combine(path, "1", "1.1"));
+            Directory.CreateDirectory(Path.Combine(path, "1", "1.1", "1.2"));
+            Directory.CreateDirectory(Path.Combine(path, "2"));
+
+            var dir2Info = new DirectoryInfo(Path.Combine(path, "2"));
+            dir2Info.Attributes = FileAttributes.Hidden;
+
+            Directory.CreateDirectory(Path.Combine(path, "3"));
+            Directory.CreateDirectory(Path.Combine(path, "3", "3.1"));
+            Directory.CreateDirectory(Path.Combine(path, "4"));
+            Directory.CreateDirectory(Path.Combine(path, "4", "4.1"));
+
+            var dir4Info = new DirectoryInfo(Path.Combine(path, "4"));
+            dir4Info.Attributes = FileAttributes.Hidden;
+
+            Directory.CreateDirectory(Path.Combine(path, "5"));
+            Directory.CreateDirectory(Path.Combine(path, "5", "5.1"));
+
+            var dir5Info = new DirectoryInfo(Path.Combine(path, "5", "5.1"));
+            dir5Info.Attributes = FileAttributes.Hidden;
+
+            Directory.CreateDirectory(Path.Combine(path, "6"));
+            File.CreateText(Path.Combine(path, "6", "6.txt")).Close();
 
         }
+
+        static void FillEmptyDirectoriesRecursive(string path, List<string> emptyDirectories)
+        {
+            if (IsEmpty(path))
+            {
+                emptyDirectories.Add(path);
+                return;
+            }
+            var subdirectories = Directory.GetDirectories(path);
+            foreach (var subdir in subdirectories)
+            {
+                FillEmptyDirectoriesRecursive(subdir, emptyDirectories);
+            }
+
+        }
+
         static bool IsEmpty(string path)
         {
             if (!Directory.Exists(path))
@@ -27,28 +92,11 @@ namespace EmptyFolderCleaner
                 if (Path.GetFileName(filename) == "desktop.ini")
                     return true;
             }
-            
+
             if (Directory.GetFiles(path).Length > 0)
                 return false;
 
-
-
             return true;
-
-        }
-        static void PrintEmptyDirectoriesRecursive(string path)
-        {
-            if (IsEmpty(path))
-            {
-                Console.WriteLine(path);
-                return;
-            }
-            var subdirectories = Directory.GetDirectories(path);
-            foreach (var subdir in subdirectories)
-            {
-                PrintEmptyDirectoriesRecursive(subdir);
-            }
-
         }
     }
 }
